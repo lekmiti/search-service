e7b3c937f24521a68bc530cc74b76d82a27f5c64
lekmiti <lekmiti1992@gmail.com>
2020-12-04 19:53:53 +0100

    configure project structure

---
+++ infrastructure/src/main/kotlin/com/lekmiti/searchservice/MainApplication.kt
@@ -0,0 +1,12 @@
+package com.lekmiti.searchservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class MainApplication
+
+fun main(args: Array<String>) {
+    runApplication<MainApplication>(*args)
+}
