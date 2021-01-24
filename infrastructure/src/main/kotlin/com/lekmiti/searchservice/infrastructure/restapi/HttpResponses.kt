e1dd607decc3261602feb1b4546bbc9978072c44
lekmiti <lekmiti1992@gmail.com>
2021-01-24 14:43:49 +0100

    add sl4j for logging + renaming + encapsulate responses in HttpResponses

--- infrastructure/src/main/kotlin/com/lekmiti/searchservice/infrastructure/restapi/HttpResponses.kt
+++ infrastructure/src/main/kotlin/com/lekmiti/searchservice/infrastructure/restapi/HttpResponses.kt
@@ -1,11 +1,27 @@
 package com.lekmiti.searchservice.infrastructure.restapi
 
+import org.slf4j.Logger
+import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
+import org.springframework.web.server.ResponseStatusException
 
 
-class HttpResponses {
-    fun <T> ok(body: T) = ResponseEntity.ok(body)
+class HttpResponses(private val log: Logger) {
 
-    fun <T> notFound(body: T, message: () -> String) = ResponseEntity.status(NOT_FOUND).body(message)
+    fun <T> ok(logMessage: String? = null, body: T): ResponseEntity<T> {
+        logMessage?.let { log.info(logMessage) }
+        return ResponseEntity.ok(body)
+    }
+
+    fun <T> notFound(logMessage: String?, leak: Boolean = false): ResponseEntity<T> {
+        logMessage?.let { log.warn(logMessage) }
+        throwResponseStatusException(NOT_FOUND, leak, logMessage)
+    }
+
+
+    private
+    fun throwResponseStatusException(status: HttpStatus, leak: Boolean = false, message: String?): Nothing {
+        throw ResponseStatusException(status, if (leak) message else null)
+    }
 }
--- infrastructure/src/main/kotlin/com/lekmiti/searchservice/infrastructure/restapi/SearchResource.kt
+++ infrastructure/src/main/kotlin/com/lekmiti/searchservice/infrastructure/restapi/SearchResource.kt
@@ -2,17 +2,19 @@ package com.lekmiti.searchservice.infrastructure.restapi
 
 import com.lekmiti.searchservice.application.SearchAppService
 import com.lekmiti.searchservice.domain.RequestModel
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Pageable
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+private val log = LoggerFactory.getLogger(SearchResource::class.java)
+private val response = HttpResponses(log)
 
 @RestController
 @RequestMapping("/api/v1/candidates")
-class CandidateResource(private val searchAppService: SearchAppService) {
+class SearchResource(private val searchAppService: SearchAppService) {
 
     @GetMapping
     fun search(
@@ -20,8 +22,8 @@ class CandidateResource(private val searchAppService: SearchAppService) {
             @RequestParam("type", required = false) type: String?,
             pageable: Pageable) =
             searchAppService.searchForCandidates(RequestModel(term, pageable)).let {
-                if (it.items.isEmpty()) ResponseEntity.notFound().build()
-                else ResponseEntity.ok(it)
+                if (it.items.isEmpty()) response.notFound("No items referenced by term: $term")
+                else response.ok("Items referenced by term '$term': ${it.items}", it)
             }
 
 }
