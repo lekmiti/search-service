6d1349212e4b4f0d86238604631df0da3ff73382
lekmiti <Lekmiti1992>
2021-02-22 00:25:25 +0100

    fix update candidate

--- infrastructure/src/main/kotlin/com/lekmiti/searchservice/infrastructure/services/ElasticCandidateService.kt
+++ infrastructure/src/main/kotlin/com/lekmiti/searchservice/infrastructure/services/ElasticCandidateService.kt
@@ -6,27 +6,25 @@ import com.lekmiti.searchservice.domain.candidate.Candidate
 import com.lekmiti.searchservice.domain.candidate.CandidateService
 import com.lekmiti.searchservice.domain.search.Item
 import com.lekmiti.searchservice.domain.search.ItemMetaData
-import com.lekmiti.searchservice.infrastructure.restapi.SearchResource
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.action.search.SearchRequest
 import org.elasticsearch.action.search.SearchResponse
+import org.elasticsearch.action.update.UpdateRequest
 import org.elasticsearch.client.RequestOptions.DEFAULT
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.common.xcontent.XContentType.JSON
 import org.elasticsearch.index.query.QueryBuilders.termQuery
 import org.elasticsearch.index.query.TermQueryBuilder
 import org.elasticsearch.index.reindex.DeleteByQueryRequest
-import org.elasticsearch.index.reindex.UpdateByQueryRequest
 import org.elasticsearch.search.SearchHit
 import org.elasticsearch.search.builder.SearchSourceBuilder
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.*
+import kotlin.reflect.full.memberProperties
 
 
 private val gson = Gson()
 
-private val log = LoggerFactory.getLogger(SearchResource::class.java)
 
 @Service
 class ElasticCandidateService(private val client: RestHighLevelClient) : CandidateService {
@@ -49,10 +47,18 @@ class ElasticCandidateService(private val client: RestHighLevelClient) : Candida
     }
 
     override fun updateCandidate(candidate: Candidate, index: String): Candidate {
-        val request = UpdateByQueryRequest(index)
-            .setQuery(TermQueryBuilder("candidateCode.keyword", candidate.candidateCode))
-        client.updateByQuery(request, DEFAULT)
+        findCandidateIdByCode(candidate.candidateCode, index)?.let {
+            val request = UpdateRequest(index, it)
+                .doc(gson.toJson(candidate), JSON)
+            client.update(request, DEFAULT)
+        }
         return candidate
+    }
+
+    private fun findCandidateIdByCode(candidateCode: CandidateCode, index: String): String? {
+        val sourceBuilder = SearchSourceBuilder().query(termQuery("candidateCode.keyword", candidateCode))
+        val searchRequest = SearchRequest().indices(index).source(sourceBuilder)
+        return client.search(searchRequest, DEFAULT).hits.hits[0]?.id
     }
 
     override fun deleteCandidate(candidateCode: CandidateCode, index: String) {
@@ -79,4 +85,7 @@ fun SearchHit.toItemMetaData() = ItemMetaData(
     highlight = highlightFields.values.map { it.fragments.toString() }.toList()
 )
 
-
+inline fun <reified T : Any> T.asMap() : Map<String, Any?> {
+    val props = T::class.memberProperties.associateBy { it.name }
+    return props.keys.associateWith { props[it]?.get(this) }
+}
